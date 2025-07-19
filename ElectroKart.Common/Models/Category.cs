4108e37a5804809d644abb329c87b5ed69be72c4
surendra-19 <surendrachagantipati@gmail.com>
2025-07-19 23:58:40 +0530

    added new tables

---
+++ ElectroKart.Common/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElectroKart.Common.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(100)")]
+        public string CategoryName { get; set; } = "";
+
+    }
+}
---
+++ ElectroKart.Common/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElectroKart.Common.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(50)")]
+        public string ProductName { get; set; } = "";
+
+        [Column(TypeName = "NVARCHAR(250)")]
+        public string ProductDescription { get; set; } = "";
+
+        [Required]
+        [Column(TypeName = "DECIMAL(15,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR(50)")]
+        public string Brand { get; set; } = "";
+
+        [ForeignKey("CategoryId")]
+        [Column("Category_Id")]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "NVARCHAR(200)")]
+        public string ImageUrl { get; set; } = "";
+
+        public Category? Category { get; set; }
+    }
+}
